036bf31286466c7dd4c370cf046f2f684565c9a8
Constance Forde <constanceforde@hotmail.com>
2024-01-02 21:47:21 +0000

    Interim CSS update

--- packages/frontend/src/NewRoutineForm.js
+++ packages/frontend/src/NewRoutineForm.js
@@ -12,21 +12,23 @@ function handleSubmit(e) {
     // onSubmit is essentially the addRoutine function from App.js
     // can also be written props.onSubmit with 'function NewRoutineForm(props)' above
     onSubmit(newItem)
-    
+
   // set setNewItem to an empty array to clear the previous item typed in
     setNewItem("")
   }
 
     return (
     <form onSubmit={handleSubmit} className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input 
-        value={newItem} 
+    {/* <div className="form-row">
+      <label htmlFor="item">New Item</label> */}
+      <input
+        className="new-item-input"
+        placeholder="Add a new routine"
+        value={newItem}
         // onChange is called every time a key is clicked
-        onChange={e => setNewItem(e.target.value)} 
+        onChange={e => setNewItem(e.target.value)}
         type="text" id="item" />
-    </div>
+    {/* </div> */}
     <button className="btn">Add</button>
     </form>
     )
