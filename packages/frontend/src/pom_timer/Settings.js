55d20ac49d6fb4df3d8d75f1cfbd9c1a5b73e54a
Constance Forde <constanceforde@hotmail.com>
2024-01-10 18:20:43 +0000

    pom timer completed

--- packages/frontend/src/pom_timer/Settings.js
+++ packages/frontend/src/pom_timer/Settings.js
@@ -1,30 +1,40 @@
-import ReactSlider from 'react-slider'
+import ReactSlider from 'react-slider' // imported slider
 import "./Slider.css"
+import SettingsContext from "./SettingsContext"
+import { useContext } from "react"
+import BackButton from "./BackButton"
 
 function Settings() {
+    const settingsInfo = useContext(SettingsContext)
     return (
         <div style={{textAlign:'left'}}>
 
-            <div className="label">work minutes:</div>
+            <div className="label">work: {settingsInfo.workMinutes}:00 minutes</div>
+                {/* slider for work minutes */}
                 <ReactSlider 
                     className={'slider'}
                     thumbClassName={'thumb'}
                     trackClassName={'track'}
-                    value={45}
+                    value={settingsInfo.workMinutes}
+                    onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
                     min={1}
                     max={120} // 2 hours
                 />
 
-            <div className="label">break minutes:</div>
+            <div className="label">break: {settingsInfo.breakMinutes}:00 minutes</div>
+                {/* slider for break minutes */}
                 <ReactSlider 
                     className={'slider green'}
                     thumbClassName={'thumb'}
                     trackClassName={'track'}
-                    value={45}
+                    value={settingsInfo.breakMinutes}
+                    onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
                     min={1}
                     max={120} // 2 hours
                 />
-
+            <div style={{textAlign:'center', marginTop:'20px'}}>
+                <BackButton onClick={() => settingsInfo.setShowSettings(false)}/>
+            </div>
         </div>
     )
 }
--- packages/frontend/src/pom_timer/Timer.js
+++ packages/frontend/src/pom_timer/Timer.js
@@ -1,31 +1,104 @@
-import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import PlayButton from "./PlayButton";
-import PauseButton from "./PauseButton";
-import SettingsButton from "./SettingsButton";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import PlayButton from "./PlayButton"
+import PauseButton from "./PauseButton"
+import SettingsButton from "./SettingsButton"
+import { useContext, useState, useEffect, useRef } from "react"
+import SettingsContext from "./SettingsContext"
 
-const red = '#f54e4e';
-const green = '#4aec8c';
+// const red = '#f54e4e';
+// const green = '#4aec8c';
 
 function Timer() {
+    const settingsInfo = useContext(SettingsContext)
+
+    const [isPaused, setIsPaused] = useState(true)
+    const [mode, setMode] = useState('work') // work/break/null
+    const [secondsLeft, setSecondsLeft] = useState(0)
+
+    const secondsLeftRef = useRef(secondsLeft)
+    const isPausedRef = useRef(isPaused)
+    const modeRef = useRef(mode)
+
+    // remove 1 second from secondsleft every time the timer component runs
+    function tick() {
+        // below is equivalent to secondsLeftRef.current = secondsLeftRef.current -1
+        secondsLeftRef.current--
+        setSecondsLeft(secondsLeftRef.current)
+    }
+
+    // the timer component runs every time settingsInfo changes
+    useEffect(() => {
+
+        function switchMode() {
+            // if the current mode is work, switch to break, otherwise switch to work
+            const nextMode = modeRef.current === 'work' ? 'break' : 'work'
+            const nextSeconds = nextMode === 'work' 
+                ? settingsInfo.workMinutes * 60
+                : settingsInfo.breakMinutes * 60
+    
+            setMode(nextMode)
+            modeRef.current = nextMode
+    
+            setSecondsLeft(nextSeconds)
+            secondsLeftRef.current = nextSeconds
+        }
+
+
+        secondsLeftRef.current = settingsInfo.workMinutes * 60;
+        setSecondsLeft(secondsLeftRef.current);
+
+
+        // when the timer starts, run this function every second, when stopped clear interval
+        const interval = setInterval(() => {
+            if (isPausedRef.current) {
+                return;  // if paused return nothing
+            }
+            if(secondsLeftRef.current === 0) {
+                return switchMode(); // if seconds left is zero, change mode
+            }
+            tick()
+        }, 1000) // speed of timer is seconds
+
+        // return a function that clears interval
+        return () => clearInterval(interval)
+    }, [settingsInfo])
+
+    const totalSeconds = mode === 'work'
+        ? settingsInfo.workMinutes * 60
+        : settingsInfo.breakMinutes * 60;
+
+    // calculate percentage of progress bar remaining
+    const percentage = Math.round(secondsLeft / totalSeconds * 100)
+
+    // calculate minutes and secconds to show in the progress bar
+    const minutes = Math.floor(secondsLeft / 60) // Math.floor rounds down
+        // if(minutes < 10) minutes =  '0' + minutes
+    let seconds = secondsLeft % 60
+        if(seconds < 10) seconds = '0' + seconds
+
+
     return (
         <div>
-            <CircularProgressbarWithChildren 
-                value={60} 
-                text={`60%`}
+            {/* React Circluar Progressbar from npmjs.com */}
+            <CircularProgressbar 
+
+                value={percentage} 
+                text={minutes + ':' + seconds}
                 styles={buildStyles({
                         // rotation, strokeLinecap,
                         textColor:'#fff',
-                        pathColor:red,
-                        trailColor:'rgba(255,255,255,.2)',
+                        pathColor:mode === 'work' ? '#f54e4e' : '#4aec8c',
+                        trailColor:'rgba(255,255,255,.2)', // grey
                         })}
             />
             <div style={{marginTop:'20px'}}>
-                <PlayButton />
-                <PauseButton />
+                {isPaused 
+                    ? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false; }} /> 
+                    : <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true; }} />} 
             </div>
             <div style={{marginTop:'20px'}}>
-                <SettingsButton />
+                <SettingsButton onClick={() => {settingsInfo.setShowSettings(true)}}/>
             </div>
         </div>
     )
