cc10a30e3233f64b607ea19059adb8f0d4d5e4d2
Constance Forde <constanceforde@hotmail.com>
2024-01-14 13:46:57 +0000

    fm uninstall

--- packages/frontend/src/sections/Navbar.jsx
+++ packages/frontend/src/sections/Navbar.jsx
@@ -11,12 +11,15 @@ const Navbar = () => (
         className={`${styles.xPaddings} py-8 relative`}
     >
         {/* <div className="absolute x-[50%] inset-0 gradient-01" /> */}
-        <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+        {/* <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
         <h1 className="font-extrabold text-[24px] leading-[30px] text-white">
             Navbar
         </h1>
+        </div> */}
 
-        </div>
+        <h1>
+            Navbar
+        </h1>
     </nav>
 )
 
